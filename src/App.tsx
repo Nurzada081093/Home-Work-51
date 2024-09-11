71933671feb01d3e8e83da6128b1140e7aa6d552
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-11 15:30:20 +0600

    There have been styling changes in the Ball.css and App.css files. Also in the App.tsx file, the output of balls through a loop was implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import "./App.css";
 import Ball from "./Components/Ball/Ball.tsx";
+import {useState} from "react";
 
 const App = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const Numbers = numbers.map((number: number, index: number) => {
+        return (
+            <Ball key={index} number={number}/>
+        );
+    });
+
+    const getNewNumbers = () => {};
+
     return (
         <div className="container">
-            <div className="ball-container">
-                <Ball number={20}/>
-                <Ball number={20}/>
-                <Ball number={20}/>
-                <Ball number={20}/>
-                <Ball number={20}/>
-            </div>
-            <button type="button" className="button">New numbers</button>
+            <div className="ball-container">{Numbers}</div>
+            <button type="button" className="button" onClick={getNewNumbers}>New numbers</button>
         </div>
     );
 };
